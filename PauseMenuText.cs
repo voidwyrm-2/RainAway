708ccfbf488197b1edf16a6ae1dfda04ab939871
Radioactivity <85001747+voidwyrm-2@users.noreply.github.com>
2024-03-14 06:29:02 -0400

    Add project files.

---
+++ PauseMenuText.cs
@@ -0,0 +1,87 @@
+﻿using Menu;
+using UnityEngine;
+
+namespace NuclearPasta.RainAway
+{
+	public static class PauseMenuText
+	{
+        //copied from https://github.com/SabreML/MusicAnnouncements/blob/master/src/PauseMenuText.cs
+
+        // The 'Currently Playing: x' text in the top right corner.
+        private static MenuLabel currentlyPlayingLabel;
+
+		public static void SetupHooks()
+		{
+			On.Menu.PauseMenu.ctor += PauseMenuHK;
+			On.Menu.PauseMenu.ShutDownProcess += PauseMenu_ShutDownProcessHK;
+			//On.HUD.TextPrompt.Draw += TextPrompt_DrawHK;
+		}
+
+		// Called when the pause menu is opened.
+		// this adds a label in the top right corner of the screen displaying its name.
+		private static void PauseMenuHK(On.Menu.PauseMenu.orig_ctor orig, PauseMenu self, ProcessManager manager, RainWorldGame game)
+		{
+			orig(self, manager, game);
+
+			float posX = game.rainWorld.options.ScreenSize.x - 10.01f;
+			float posY = game.rainWorld.options.ScreenSize.y - 13f;
+
+			currentlyPlayingLabel = new MenuLabel(self, self.pages[0], $"Rain Away is currently {Plugin.status}", new Vector2(posX, posY), Vector2.zero, true);
+			currentlyPlayingLabel.label.color = Menu.Menu.MenuRGB(Menu.Menu.MenuColors.MediumGrey);
+			currentlyPlayingLabel.label.alignment = FLabelAlignment.Right; // Align on the right so that longer song names don't go off the screen.
+			currentlyPlayingLabel.label.alpha = 0f; // Start out invisible in order to fade in.
+
+			self.pages[0].subObjects.Add(currentlyPlayingLabel);
+			Debug.Log($"(RainAway) Rain Away is currently {Plugin.status}");
+		}
+
+		private static void PauseMenu_ShutDownProcessHK(On.Menu.PauseMenu.orig_ShutDownProcess orig, PauseMenu self)
+		{
+			if (currentlyPlayingLabel != null)
+			{
+				RemoveCurrentlyPlayingText(); // Called before `orig()` so that it doesn't waste time trying to remove itself twice.
+			}
+			orig(self);
+		}
+
+		/*
+		// Called when the "Paused" text at the bottom left of the screen updates its appearance. (Around every frame)
+		// This is used to steal its alpha value and fade in along with it, or to smoothly fade out if the song ends.
+		private static void TextPrompt_DrawHK(On.HUD.TextPrompt.orig_Draw orig, HUD.TextPrompt self, float timeStacker)
+		{
+			orig(self, timeStacker);
+			if (currentlyPlayingLabel == null) // No text to modify.
+			{
+				return;
+			}
+
+			float newAlpha;
+			if (MusicAnnouncementsMod.SongToAnnounce != null)
+			{
+				newAlpha = self.label.alpha; // Fade in.
+			}
+			else // If the text exists but there's no song, then it ended while the menu was open.
+			{
+				newAlpha = RWCustom.Custom.LerpAndTick(currentlyPlayingLabel.label.alpha, 0f, 0.02f, 0.02f); // Fade out.
+				if (newAlpha <= 0f)
+				{
+					Debug.Log("(MusicAnnouncements) Song ended, removing menu text");
+					RemoveCurrentlyPlayingText();
+					return;
+				}
+			}
+
+			currentlyPlayingLabel.label.alpha = newAlpha;
+			labelMusicSprite.alpha = newAlpha;
+		}
+		*/
+
+		// Clean up our variables.
+		private static void RemoveCurrentlyPlayingText()
+		{
+			currentlyPlayingLabel.RemoveSprites();
+			currentlyPlayingLabel.owner.RemoveSubObject(currentlyPlayingLabel);
+			currentlyPlayingLabel = null;
+		}
+	}
+}
---
+++ Plugin.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using BepInEx;
+using ImprovedInput;
+using UnityEngine;
+
+namespace NuclearPasta.RainAway
+{
+    [BepInDependency("com.dual.improved-input-config", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInPlugin("nc.rainaway", "Rain Away!", "1.0.0")]
+    class Plugin : BaseUnityPlugin
+    {
+
+        public static bool debug = true;
+
+        public static bool isenabled = true;
+        public static string status = "enabled";
+
+        public static bool isStory = false;
+        public static int cycleLength = 0;
+        public static int cycleLimit = 0;
+        public static float cCycleTime = 0;
+        public static bool reset = false;
+
+        public static PlayerKeybind ToggleRainAway;
+
+        public static bool ImprovedInputEnabled;
+
+        public static bool isInit;
+
+        // Add hooks
+        public void OnEnable()
+        {
+            try
+            {
+                ToggleRainAway = PlayerKeybind.Register("rainaway:togglerainaway", "Rain Away", "Rain Away toggle", KeyCode.Alpha0, KeyCode.JoystickButton3);
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+            }
+
+            On.RainWorld.PostModsInit += RainWorld_PostModsInit;
+            //On.World.ctor += World_ctor;
+            On.Player.Update += Player_Update;
+            On.GlobalRain.Update += GlobalRain_Update;
+            On.RainWorld.OnModsInit += RainWorld_OnModsInit;
+            
+        }
+
+        private void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+        {
+            try
+            {
+                if (!isInit)
+                {
+                    isInit = true;
+                    ToggleRainAway.Description = "The key pressed to toggle Rain Away";
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+            }
+            finally
+            {
+                orig.Invoke(self);
+            }
+        }
+
+        private void RainWorld_PostModsInit(On.RainWorld.orig_PostModsInit orig, RainWorld self)
+        {
+            ImprovedInputEnabled = ModManager.ActiveMods.Exists((ModManager.Mod mod) => mod.id == "improved-input-config");
+            if (!ImprovedInputEnabled)
+            {
+                ImprovedInputEnabled = ModManager.ActiveMods.Exists((ModManager.Mod mod) => mod.id == "com.dual.improved-input-config");
+                Logger.LogDebug("no mod with id \"improved-input-config\"\" found, trying \"com.dual.improved-input-config\" instead...");
+            }
+            if (ImprovedInputEnabled)
+            {
+                Logger.LogDebug("mod with id \"com.dual.improved-input-config\" found, continuing as normal");
+            }
+            else
+            {
+                Logger.LogDebug("no mod with id \"com.dual.improved-input-config\" found, continuing without");
+            }
+            orig(self);
+        }
+
+        private void GlobalRain_Update(On.GlobalRain.orig_Update orig, GlobalRain self)
+        {
+            if (reset) self.ResetRain(); reset = false;
+            orig(self);
+        }
+
+        private void Player_Update(On.Player.orig_Update orig, Player self, bool eu)
+        {
+
+            if (isenabled)
+            {
+                status = "enabled";
+            }
+            else
+            {
+                status = "disabled";
+            }
+
+            if (ToggleRainAway.CheckRawPressed(0) && isenabled)
+            {
+                isenabled = false;
+            }
+            else if (ToggleRainAway.CheckRawPressed(0) && !isenabled)
+            {
+                isenabled = true;
+            }
+
+            cycleLimit = cycleLength - (cycleLength / 2);
+            if (cCycleTime < cycleLimit && isenabled) reset = true;
+            if (debug)
+            {
+                string m1 = "isStory:" + isStory.ToString(); Logger.LogInfo(m1);
+                string m2 = "cycleLength:" + cycleLength.ToString(); Logger.LogInfo(m2);
+                string m3 = "cycleLimit:" + cycleLimit.ToString(); Logger.LogInfo(m3);
+                string m4 = "cCycleTime:" + cCycleTime.ToString(); Logger.LogInfo(m4);
+                string m5 = "reset:" + reset.ToString(); Logger.LogInfo(m5);
+            }
+            try
+            {
+                isStory = self.room.game.IsStorySession;
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e);
+            }
+
+            try
+            {
+                cycleLength = self.room.world.rainCycle.cycleLength;
+                cCycleTime = self.room.world.rainCycle.AmountLeft * cycleLength;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+            }
+
+            orig(self, eu);
+        }
+
+        /*
+        private void World_ctor(On.World.orig_ctor orig, World self, RainWorldGame game, Region region, string name, bool singleRoomWorld)
+        {
+            try
+            {
+                cycleLength = self.rainCycle.cycleLength;
+                cCycleTime = (int) self.rainCycle.AmountLeft;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex);
+            }
+            
+            orig(self, game, region, name, singleRoomWorld);
+        }
+        */
+    }
+}
